7e083a4e89e38470386cb7c38d1c8ada8a7c8475
Jeter <35966325+JeterG@users.noreply.github.com>
2022-05-29 02:26:55 -0400

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,4 +1,10 @@
-import time 
-sleep_time = input("How long do you want to wait before running?: ")
-time.sleep(int(sleep_time))
+import time
+
+sleep_time = int(input("How long do you want to wait before running?: "))
+
+while sleep_time > 0:
+
+    print(sleep_time, end="\r")
+    time.sleep(1)
+    sleep_time -= 1
 print("Time to go for a run!!!")
